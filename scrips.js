a1ee4b099d908bf434053d55e761154cb91171b1
leoebahamonde@gmail.com <leoebahamonde@gmail.com>
2025-10-01 08:58:50 -0300

    agrego btn borrar ingresos

--- scrips.js
+++ scrips.js
@@ -1,14 +1,14 @@
 // Variables
 
 //boton bloqueado egresos
-const inputDescripcion = document.querySelector('.inputDescripcion');
-const inputImportes = document.querySelector('.inputImportes');
-const btnGuardar = document.querySelector('.btnGuardar');
+const inputDescripcion = document.querySelector(".inputDescripcion");
+const inputImportes = document.querySelector(".inputImportes");
+const btnGuardar = document.querySelector(".btnGuardar");
 
 //boton bloqueado ingresos
-const inputDesIngreso = document.querySelector('.input-des-ingreso');
-const inputImporteIngreso = document.querySelector('.input-importe-ingreso');
-const btnIngreso = document.querySelector('.btn-ingresos');
+const inputDesIngreso = document.querySelector(".input-des-ingreso");
+const inputImporteIngreso = document.querySelector(".input-importe-ingreso");
+const btnIngreso = document.querySelector(".btn-ingresos");
 
 const tbodyIngresos = document.querySelector(".resultadoIngreso");
 const tbodyEgresos = document.querySelector(".resultadoEgreso");
@@ -22,8 +22,8 @@ let importesIngresos = document.querySelector(".importesIngresos");
 let descripcionEgresos = document.querySelector(".descripcionEgresos");
 let importesEgresos = document.querySelector(".importesEgresos");
 
-const btnGuardarEgresos = document.querySelector('.btn-guardarEgresos');
-const btnGuardarIngresos = document.querySelector('.btn btn-guardarIngresos');
+const btnGuardarEgresos = document.querySelector(".btn-guardarEgresos");
+const btnGuardarIngresos = document.querySelector(".btn btn-guardarIngresos");
 
 let ingresos = [];
 let egresos = [];
@@ -32,43 +32,43 @@ let egresos = [];
 ingresoSection.addEventListener("click", guardar);
 egresoSection.addEventListener("click", guardarEgresos);
 
-inputDescripcion.addEventListener('input',validar);
-inputImportes.addEventListener('input',validar);
-
+inputDescripcion.addEventListener("input", validar);
+inputImportes.addEventListener("input", validar);
 
 //eventos de validaciones
 
-function validar(){
-  if (inputDescripcion.value.trim() !== "" && inputImportes.value.trim() !== ""){
-    btnGuardar.disabled = false
-    btnGuardar.classList.add('btn-enabled')
-    btnGuardar.classList.remove('btn-disabled') 
+function validar() {
+  if (
+    inputDescripcion.value.trim() !== "" &&
+    inputImportes.value.trim() !== ""
+  ) {
+    btnGuardar.disabled = false;
+    btnGuardar.classList.add("btn-enabled");
+    btnGuardar.classList.remove("btn-disabled");
+  } else {
+    btnGuardar.disabled = true;
+    btnGuardar.classList.remove("btn-enabled");
+    btnGuardar.classList.add("btn-disabled");
   }
-  else{
-    btnGuardar.disabled = true
-    btnGuardar.classList.remove('btn-enabled')
-    btnGuardar.classList.add('btn-disabled') ;
-  }
-
 }
 
 // bloquear boton ingreso
 
-inputDesIngreso.addEventListener('input',validarIngreso)
-inputImporteIngreso.addEventListener('input',validarIngreso)
+inputDesIngreso.addEventListener("input", validarIngreso);
+inputImporteIngreso.addEventListener("input", validarIngreso);
 
-
-function validarIngreso(){
-  if (inputDesIngreso.value.trim() !== "" && inputImporteIngreso.value.trim() !== "") {
+function validarIngreso() {
+  if (
+    inputDesIngreso.value.trim() !== "" &&
+    inputImporteIngreso.value.trim() !== ""
+  ) {
     btnIngreso.disabled = false;
     btnIngreso.classList.remove("btn-disabled");
-    btnIngreso.classList.add('btn-enabled');  
-  }
-  else{
+    btnIngreso.classList.add("btn-enabled");
+  } else {
     btnIngreso.disabled = true;
-    btnIngreso.classList.remove('btn-enabled');  
+    btnIngreso.classList.remove("btn-enabled");
     btnIngreso.classList.add("btn-disabled");
-  
   }
 }
 
@@ -81,10 +81,9 @@ function guardar(e) {
 }
 
 function guardarEgresos(e) {
-
   if (e.target.classList.contains("btn-guardarEgresos")) {
     const fila = e.target.closest("tr");
-    leeDatosEgresos(fila); 
+    leeDatosEgresos(fila);
   }
 }
 
@@ -103,12 +102,10 @@ function leeDatosEgresos(fila) {
   (descripcionEgresos.value = ""), (importesEgresos.value = "");
 
   //bloquea el boton de guardado
-  validar()
-  
+  validar();
 }
 
 function leeDatos() {
-  
   let campos = {
     fecha: new Date().toLocaleDateString(),
     descripcionIngresos: descripcionIngresos.value,
@@ -118,11 +115,9 @@ function leeDatos() {
   ingresos = [...ingresos, campos];
   mostrarDatos();
 
-  
-  
   (descripcionIngresos.value = ""), (importesIngresos.value = "");
   //bloquea el btn guardado
-  validarIngreso()
+  validarIngreso();
 }
 
 function mostrarDatos() {
@@ -134,9 +129,13 @@ function mostrarDatos() {
     const row = document.createElement("tr");
     const { fecha, descripcionIngresos, importesIngresos } = item;
     row.innerHTML = `
-    <td>${fecha}</td>
-    <td>${descripcionIngresos}</td>
-    <td>${importesIngresos}</td>
+    <td><b>${fecha}</b></td>
+    <td><b>${descripcionIngresos}</b></td>
+    <td><b>${importesIngresos}</b></td>
+     <td>
+      <button class="btn btn-borrar">Borrar</button>
+      <button class="btn btn-editar">Editar</button>
+    </td>
     `;
     tbodyIngresos.appendChild(row);
   });
@@ -148,11 +147,28 @@ function mostrarDatosEgresos() {
     const row = document.createElement("tr");
     const { fecha, descripcionEgresos, importesEgresos } = item;
     row.innerHTML = `
-    <td>${fecha}</td>
-    <td>${descripcionEgresos}</td>
-    <td>${importesEgresos}</td>
+    <td><b>${fecha}</b></td>
+    <td><b>${descripcionEgresos}</b></td>
+    <td><b>${importesEgresos}</b></td>
+    <td>
+      <button class="btn btn-borrar">Borrar</button>
+      <button class="btn btn-editar">Editar</button>
+    </td>
     `;
 
     tbodyEgresos.appendChild(row);
   });
+}
+//borrar ingreso de la tabla
+
+tbodyIngresos.addEventListener('click', borrarIngreso)
+
+function borrarIngreso(e){
+  if(e.target.classList.contains('btn-borrar')){
+   const fila = e.target.closest('tr'); // saco la fila entera
+    const index = Array.from(tbodyIngresos.children).indexOf(fila); // posición de la fila en el array
+    
+    ingresos.splice(index, 1); // elimino del array
+    mostrarDatos(); // repinto la tabla sin ese registro
+  }
 }
